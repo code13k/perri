7c58acf5a57ce717a26ebaed79c9457ff6ff533e
WikiDru <wikidru@gmail.com>
2018-09-27 01:02:16 +0900

    Modify timer name

--- src/main/java/org/code13k/perri/app/Status.java
+++ src/main/java/org/code13k/perri/app/Status.java
@@ -42,7 +42,7 @@ public class Status {
      */
     public void init() {
         // Timer
-        Timer timer = new Timer("perri-status-logging");
+        Timer timer = new Timer("perri-status");
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
