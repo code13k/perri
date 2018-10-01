2ebcd83387b7785df8607d787b122796ae1348ff
WikiDru <wikidru@gmail.com>
2018-10-02 01:37:41 +0900

    [#15] Add API : /app/config

--- src/main/java/org/code13k/perri/service/api/controller/AppAPI.java
+++ src/main/java/org/code13k/perri/service/api/controller/AppAPI.java
@@ -2,19 +2,13 @@ package org.code13k.perri.service.api.controller;
 
 import org.code13k.perri.app.Env;
 import org.code13k.perri.app.Status;
-import org.code13k.perri.business.message.MessageManager;
+import org.code13k.perri.config.AppConfig;
+import org.code13k.perri.model.config.app.PortInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 public class AppAPI extends BasicAPI {
     // Logger
@@ -32,6 +26,16 @@ public class AppAPI extends BasicAPI {
      */
     public String status() {
         return toResultJsonString(Status.getInstance().values());
+    }
+
+    /**
+     * config
+     */
+    public String config(){
+        PortInfo portInfo = AppConfig.getInstance().getPort();
+        Map<String, Object> result = new HashMap<>();
+        result.put("port", portInfo.toMap());
+        return toResultJsonString(result);
     }
 
     /**
